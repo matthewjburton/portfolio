290f34e882d97c52d90ac6312b34bdc381005bcd
Matt Burton <mattjburton64@gmail.com>
2024-01-30 19:29:57 -0500

    local react app working

---
+++ src/Components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+return (
+    <div className="flex flex-col m-10">
+        <div className="m-10">one</div>
+        <div>two</div>
+    </div>
+);
+}
+
+export default Home;
