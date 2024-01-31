75be4c50e6a2c38782aafea924cad812c155667f
Matt Burton <mattjburton64@gmail.com>
2024-01-30 20:25:15 -0500

    tailwind installed and working

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Home: React.FC = () => {
-return (
-    <div className="flex flex-col m-10">
-        <div className="m-10">one</div>
-        <div>two</div>
-    </div>
-);
+    return (
+        <>
+            <div className="flex self-center bg-blue-500 text-white p-4">
+                This is styled using Tailwind CSS!
+            </div>
+            <div style={{ color: 'red' }}>This is styled with CSS</div>
+        </>
+    );
 }
 
 export default Home;
