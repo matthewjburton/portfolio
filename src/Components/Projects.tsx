106b91d3e78b341c86e3aff1f02756a829b85ee0
matthewjburton <mattjburton64@gmail.com>
2024-06-28 23:48:21 -0400

    projects section start

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,10 +1,59 @@
 import React from "react";
+import Yellowstone from "../Images/Yellowstone.png";
+import Overcome from "../Images/Overcome.png";
+import AllStars from "../Images/All Stars.png";
+import ForeverLit from "../Images/Forever Lit.png";
 
 const Projects: React.FC = () => {
     return (
-        <div className="bg-gray-800 flex justify-center">
-            <div className="text-white font-bold text-xl p-8">
-                Projects
+        <div className="flex w-full justify-center ">
+            <div className="bg-slate-800 text-white flex flex-col items-center w-2/3 p-12 gap-12">
+                <div className="text-3xl font-bold">Projects</div>
+                <div>
+                    <div className="text-2xl font-bold py-4">Programs</div>
+                    <div>
+                        <a href="https://github.com/matthewjburton/comfort-airlines">
+                            Comfort Airlines</a>
+                        <p>For my senior capstone project our class was divided into teams of six and instructed to
+                            design a software solution for
+                            the fictional company Comfort Airlines. We were given numerous constraints, requirements,
+                            and challenges which we had to
+                            decide how to solve and implement. Out of the five teams in the class, our group received
+                            the class nomination for the
+                            project most worthy of winning the company's "bid".</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="text-2xl font-bold py-4">Games</div>
+                    <div className="grid grid-flow-row grid-cols-2 gap-4">
+                        <div>
+                            <iframe title="Endless Breakout" frameBorder="0"
+                                src="https://itch.io/embed/303595?bg_color=811515&amp;fg_color=FFF&amp;link_color=fa5c5c&amp;border_color=ce5555"
+                                width="208" height="167"><a href="https://matt-burton.itch.io/endless-breakout">Endless
+                                    Breakout by Mattburton64</a></iframe>
+                            <p>Infinite arcade brick breaker</p>
+                        </div>
+                        <div>
+                            <iframe title="Head Hunter" frameBorder="0"
+                                src="https://itch.io/embed/1437587?bg_color=1E1E1E&amp;fg_color=ffffff&amp;link_color=fa5c5c&amp;border_color=7d7d7d"
+                                width="208" height="167"><a href="https://matt-burton.itch.io/head-hunter">Head Hunter
+                                    by
+                                    Mattburton64</a></iframe>
+                            <p>An action roguelike platformer</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="text-2xl font-bold py-4">Graphics</div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <img className="w-80 h-80 object-fit" src={Yellowstone} alt="Yellowstone" />
+                        <img className="w-80 h-80 object-fit" src={Overcome} alt="Overcome" />
+                        <img className="w-80 h-80 object-fit" src={ForeverLit} alt="Forever Lit" />
+                        <img className="w-80 h-80 object-fit" src={AllStars} alt="All Stars" />
+                    </div>
+                </div>
             </div>
         </div>
     );
