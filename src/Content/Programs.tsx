71f7e4ed3e7e31d3dcaf87128380946c4a772b6c
matthewjburton <mattjburton64@gmail.com>
2024-07-01 13:10:45 -0400

    added two programs

--- src/Content/Programs.tsx
+++ src/Content/Programs.tsx
@@ -2,7 +2,6 @@ export const programs = [
     {
         id: 1,
         title: 'Comfort Airlines',
-        icon: "fa-solid fa-plane-up",
         description:
             'Generates flight plans and simulates airline operations for evaluation and profit optimization. Tech stack includes Python, MariaDB, and Docker.',
         link: 'https://github.com/matthewjburton/comfort-airlines',
@@ -10,10 +9,23 @@ export const programs = [
     {
         id: 2,
         title: 'Business Case Form',
-        icon: "fa-solid fa-briefcase",
         description:
             "A web app for evaluating potential business investments. Tech stack includes React, Apollo, Hasura, and PostgreSQL. UI created in Figma.",
         link: 'https://udig-business-case-form-test.netlify.app/',
     },
+    {
+        id: 3,
+        title: 'Fine Dining Bot',
+        description:
+            "A webscraping bot for making reservations at High Point Uniersity's fine dining restaurants. Built with Python and Selenium.",
+        link: 'https://github.com/matthewjburton/reservation-bot',
+    },
+    {
+        id: 4,
+        title: 'Blackjack',
+        description:
+            'A CLI Blackjack game. Built in C++.',
+        link: 'https://github.com/matthewjburton/blackjack',
+    }
     // Add more programs as needed
 ];
