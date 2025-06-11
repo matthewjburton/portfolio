9e1a838a643a8f76e8280fc0cc183c98cc8669a5
Matt Burton <mattjburton64@gmail.com>
2025-06-11 10:10:20 -0400

    converted to vite with working tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit', // Enable JIT mode
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  darkMode: 'media',
+  mode: "jit", // Enable JIT mode
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media",
   theme: {
     extend: {
       colors: {
         background: {
-          DEFAULT: '#FFFFFF',
-          dark: '#101010',
+          DEFAULT: "#FFFFFF",
+          dark: "#101010",
         },
         container: {
-          DEFAULT: '#f4f4f4',
-          dark: '#1a1a1e',
+          DEFAULT: "#f4f4f4",
+          dark: "#1a1a1e",
         },
         dropdownHover: {
-          DEFAULT: '#f4f4f4',
-          dark: '#404040',
+          DEFAULT: "#f4f4f4",
+          dark: "#404040",
         },
         primaryText: {
-          DEFAULT: '#121212',
-          dark: '#FFFFFF',
+          DEFAULT: "#121212",
+          dark: "#FFFFFF",
         },
         secondaryText: {
-          DEFAULT: '#262626',
-          dark: '#DEDEDE',
+          DEFAULT: "#262626",
+          dark: "#DEDEDE",
         },
         tertiaryText: {
-          DEFAULT: '#787878',
-          dark: '#BEBEBE',
+          DEFAULT: "#787878",
+          dark: "#BEBEBE",
         },
         accent: {
-          DEFAULT: '#44D3AB',
-          dark: '#00b294',
+          DEFAULT: "#44D3AB",
+          dark: "#00b294",
         },
       },
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
   },
