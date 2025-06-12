d500e9c92365393d0b03dddb9bd9bf24c860d43b
Matt Burton <mattjburton64@gmail.com>
2025-06-12 09:31:55 -0400

    simplified skills section, added title and degree to bio. removed sub headers

--- frontend/src/Components/Projects/ProjectContainer.tsx
+++ frontend/src/Components/Projects/ProjectContainer.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Link from './Link'
 
-interface ProgramContainerProps {
-  program: {
+interface ProjectContainerProps {
+  project: {
     id: number
     title: string
     description: string
@@ -12,29 +12,29 @@ interface ProgramContainerProps {
   }
 }
 
-const ProgramContainer: React.FC<ProgramContainerProps> = ({ program }) => {
+const ProjectContainer: React.FC<ProjectContainerProps> = ({ project }) => {
   return (
     <>
       <div
-        key={program.id}
+        key={project.id}
         className="bg-container dark:bg-container-dark group flex flex-col items-center justify-center overflow-hidden rounded-lg p-8 shadow-xl transition duration-300 ease-in-out hover:shadow-[0_0_10px_#8fcf45] hover:dark:shadow-[0_0_10px_#82db4f]"
       >
         <div className="flex h-full w-full flex-col items-center justify-between gap-4 rounded-lg p-4 lg:gap-8 lg:p-8">
           <div className="flex w-full flex-col items-start gap-4">
             <div className="flex flex-col gap-2">
               <div className="text-start text-xl font-bold md:text-2xl lg:text-3xl">
-                {program.title}
+                {project.title}
               </div>
 
-              <span className="text-tertiary-text dark:text-tertiary-text-dark text-sm italic">
-                {program.year.toString()}
+              <span className="text-secondary-text dark:text-secondary-text-dark text-sm italic">
+                {project.year.toString()}
               </span>
             </div>
 
-            <div className="text-lg">{program.description}</div>
+            <div className="text-lg">{project.description}</div>
 
             <div className="flex flex-wrap gap-2">
-              {program.tags.map((tag) => (
+              {project.tags.map((tag) => (
                 <span
                   key={tag.label}
                   className="border-accent text-gradient-accent hover:bg-accent flex items-center gap-1 rounded-full border px-4 py-2 text-sm transition hover:scale-105"
@@ -48,7 +48,7 @@ const ProgramContainer: React.FC<ProgramContainerProps> = ({ program }) => {
             </div>
           </div>
           <div className="flex w-full items-center justify-end">
-            <Link link={program.link} text="More" />
+            <Link link={project.link} text="More" />
           </div>
         </div>
       </div>
@@ -56,4 +56,4 @@ const ProgramContainer: React.FC<ProgramContainerProps> = ({ program }) => {
   )
 }
 
-export default ProgramContainer
+export default ProjectContainer
