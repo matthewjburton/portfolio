17b441c7fff8ea0d06b13027b8cad311c9cc23dc
Matt Burton <mattjburton64@gmail.com>
2025-06-12 14:27:33 -0400

    Updates

--- src/Components/Projects/useSortedProjectsHook.ts
+++
@@ -1,6 +0,0 @@
-import { projects } from '../../Content/projects'
-
-export const useSortedProjects = () => {
-  const sorted = [...projects].sort((a, b) => b.year - a.year)
-  return { projects: sorted }
-}
